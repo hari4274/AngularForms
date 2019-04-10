83642e01e6f6f998d244116c484a57a468b85607
Hariprasath <bala.hariprasath@gmail.com>
2019-04-10 22:46:03 +0530

    Angular Forms Tutorial - 6 - Binding Data to a Model(oneway binding)

--- TemplateDrivenForms/tdf/src/app/app.component.ts
+++ TemplateDrivenForms/tdf/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'tdf';
   topics = ['Angular', 'React', 'Vue'];
+  userModel = new User('Rob', 'a@gmail.com', 966664444, '', 'morning', true);
 }
