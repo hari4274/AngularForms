9fa32a4b815937d138b8f893d30a89e22b3204ea
Hariprasath <bala.hariprasath@gmail.com>
2019-04-11 22:02:58 +0530

    Angular Forms Tutorial - 18 - Creating the Form Model

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-forms';
+  registrationForm = new FormGroup({
+    userName: new FormControl('Hari'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
 }
