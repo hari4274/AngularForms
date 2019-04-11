e796fc588f260a001d18d4fce8acece1c24916ff
Hariprasath <bala.hariprasath@gmail.com>
2019-04-11 23:14:41 +0530

    Angular Forms Tutorial - 21 - FormBuilder Service

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +8,29 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class AppComponent {
   title = 'reactive-forms';
-  registrationForm = new FormGroup({
-    userName: new FormControl('Hari'),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    address : new FormGroup({
-      city : new FormControl(''),
-      state : new FormControl(''),
-      postalCode : new FormControl('')
-    })
-  });
+
+  constructor (private fb: FormBuilder) {}
+
+    registrationForm = this.fb.group({
+      userName: ['Hari'],
+      password: [''],
+      confirmPassword: [''],
+      address: this.fb.group({
+        city : [''],
+        state : [''],
+        postalCode : ['']
+      }),
+    });
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Hari'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address : new FormGroup({
+  //     city : new FormControl(''),
+  //     state : new FormControl(''),
+  //     postalCode : new FormControl('')
+  //   })
+  // });
 
   loadApiData() {
     this.registrationForm.patchValue({
